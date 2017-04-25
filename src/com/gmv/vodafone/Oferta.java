0926f6f93f134438c99367bc049c1cabe2d0a01e
zelaya <ticocoto8@hotmail.com>
2017-04-25 09:49:55 +0200

    modificacion oferta.java agregando toString

--- src/com/gmv/vodafone/Oferta.java
+++ src/com/gmv/vodafone/Oferta.java
@@ -35,6 +35,12 @@ public class Oferta implements Serializable{
 	public void setFechaInicio(LocalDate fechaInicio) {
 		this.fechaInicio = fechaInicio;
 	}
+
+	@Override
+	public String toString() {
+		return "Oferta [codigoOferta=" + codigoOferta + ", referencia=" + referencia + ", fechaInicio=" + fechaInicio
+				+ "]";
+	}
 	
 	
 
